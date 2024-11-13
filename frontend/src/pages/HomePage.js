71acb19148ed9843944750ac0b89f7ef266f68ab
Ananya <ananyachauhan647@gmail.com>
2024-11-13 20:20:27 +0530

    design

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,9 +1,7 @@
-import React from 'react';
 import Hero from '../components/Hero';
 import ExploreHeritage from '../components/ExploreHeritage';
 import Map from '../components/Map';
-import ImageCollage from '../components/ImageCollage';
-import ImageGrid from '../components/ImageGrid'; // Importing ImageGrid component
+import ImageGrid from '../components/ImageGrid';
 import Testimonial from '../components/Testimonial';
 
 const HomePage = () => {
@@ -17,33 +15,24 @@ const HomePage = () => {
         <ExploreHeritage />
       </section>
 
+      {/* Full-viewport Map Section */}
       <section
-        id="map-and-collage"
+        id="map"
         style={{
-          display: "flex",
           width: "100vw",
           height: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
         }}
       >
-        {/* Left Side - Map Component */}
-        <div style={{ flex: 1, height: "100vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
-          <div style={{ width: "100%", height: "100%" }}>
-            <Map />
-          </div>
-        </div>
-
-        {/* Right Side - Image Collage */}
-        <div style={{ flex: 1, height: "100vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
-          <div style={{ width: "100%", height: "100%" }}>
-            <ImageCollage />
-          </div>
-        </div>
+        <Map />
       </section>
 
-      {/* Adding ImageGrid section above Testimonial */}
-      <section id="image-grid" className="py-12">
+      <section id="explore-heritage">
         <ImageGrid />
       </section>
+
 
       <section id="testimonials" className="py-12">
         <Testimonial />
