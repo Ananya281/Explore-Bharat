675aab4664aaee2b8c59eb0733e2afdcc289df77
Medhansh223 <medhanshjainfor@gmail.com>
2024-11-27 20:39:26 +0530

    g

--- backend/server.js
+++ backend/server.js
@@ -34,5 +34,12 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 app.use('/api/auth', authRoutes);
 
 // Start the server
+
+app.get('/',(req,res)=>{
+  return res.json({
+    message:"Server UP"
+  })
+})
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
