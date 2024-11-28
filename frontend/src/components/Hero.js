41080244d09a6c898c216621f8bfd1ace59b7a75
Medhansh223 <medhanshjainfor@gmail.com>
2024-11-28 21:11:59 +0530

    patni

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -49,6 +49,7 @@ const handleSearchSubmit = async (e) => {
         {
           headers: {
             'Content-Type': 'multipart/form-data',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
           },
           // withCredentials: true, // Allow credentials (if required by your backend)
         }
